c24d6fc6284d75197093f4fc1a10b9a8b315e432
Javier Cejudo <javier@javiercejudo.com>
2015-07-18 10:32:14 +1000

    refactor(metric-prefixes): separated as external module

--- src/linear-presets.js
+++ src/linear-presets.js
@@ -4,6 +4,7 @@
 
 var presets = require('../data/presets.json');
 
+presets.metricPrefixes = require('linear-presets-metric-prefixes')
 presets.temperature = require('linear-presets-temperature')
 presets.temperatureDifference = require('linear-presets-temperature-difference')
 
--- test/presets.js
+++ test/presets.js
@@ -16,48 +16,28 @@ var invert = function invert(preset) {
 
 describe('built-in presets', function() {
   it('should include metric prefixes', function() {
-    var metric = presets.metricPrefixes;
-
-    (1).should.be.approximately(convert(1e-24, invert(metric.noneToYotta)), 1e-15, 'noneToYotta')
-      .and.approximately(convert(1e-21, invert(metric.noneToZetta)), 1e-15, 'noneToZetta')
-      .and.exactly(convert(1e-15, invert(metric.noneToPeta)), 'noneToPeta')
-      .and.exactly(convert(1e-12, invert(metric.noneToTera)), 'noneToTera')
-      .and.exactly(convert(1e-9, invert(metric.noneToGiga)), 'noneToGiga')
-      .and.exactly(convert(1e-6, invert(metric.noneToMega)), 'noneToMega')
-      .and.exactly(convert(1e-3, invert(metric.noneToKilo)), 'noneToKilo')
-      .and.exactly(convert(0.01, invert(metric.noneToHecto)), 'noneToHecto')
-      .and.exactly(convert(0.1, invert(metric.noneToDeca)), 'noneToDeca')
-      .and.exactly(convert(10, invert(metric.noneToDeci)), 'noneToDeci')
-      .and.exactly(convert(100, invert(metric.noneToCenti)), 'noneToCenti')
-      .and.exactly(convert(1e3, invert(metric.noneToMilli)), 'noneToMilli')
-      .and.exactly(convert(1e6, invert(metric.noneToMicro)), 'noneToMicro')
-      .and.exactly(convert(1e9, invert(metric.noneToNano)), 'noneToNano')
-      .and.exactly(convert(1e12, invert(metric.noneToPico)), 'noneToPico')
-      .and.exactly(convert(1e15, invert(metric.noneToFemto)), 'noneToFemto')
-      .and.exactly(convert(1e18, invert(metric.noneToAtto)), 'noneToAtto')
-      .and.exactly(convert(1e21, invert(metric.noneToZepto)), 'noneToZepto')
-      .and.exactly(convert(1e24, invert(metric.noneToYocto)), 'noneToYocto');
-
-    (0).should.be.exactly(convert(0, metric.noneToYotta), 'noneToYotta')
-      .and.exactly(convert(0, metric.noneToZetta), 'noneToZetta')
-      .and.exactly(convert(0, metric.noneToExa), 'noneToExa')
-      .and.exactly(convert(0, metric.noneToPeta), 'noneToPeta')
-      .and.exactly(convert(0, metric.noneToTera), 'noneToTera')
-      .and.exactly(convert(0, metric.noneToGiga), 'noneToGiga')
-      .and.exactly(convert(0, metric.noneToMega), 'noneToMega')
-      .and.exactly(convert(0, metric.noneToKilo), 'noneToKilo')
-      .and.exactly(convert(0, metric.noneToHecto), 'noneToHecto')
-      .and.exactly(convert(0, metric.noneToDeca), 'noneToDeca')
-      .and.exactly(convert(0, metric.noneToDeci), 'noneToDeci')
-      .and.exactly(convert(0, metric.noneToCenti), 'noneToCenti')
-      .and.exactly(convert(0, metric.noneToMilli), 'noneToMilli')
-      .and.exactly(convert(0, metric.noneToMicro), 'noneToMicro')
-      .and.exactly(convert(0, metric.noneToNano), 'noneToNano')
-      .and.exactly(convert(0, metric.noneToPico), 'noneToPico')
-      .and.exactly(convert(0, metric.noneToFemto), 'noneToFemto')
-      .and.exactly(convert(0, metric.noneToAtto), 'noneToAtto')
-      .and.exactly(convert(0, metric.noneToZepto), 'noneToZepto')
-      .and.exactly(convert(0, metric.noneToYocto), 'noneToYocto');
+    presets.metricPrefixes.should.have.keys([
+      'noneToYotta',
+      'noneToZetta',
+      'noneToExa',
+      'noneToPeta',
+      'noneToTera',
+      'noneToGiga',
+      'noneToMega',
+      'noneToKilo',
+      'noneToHecto',
+      'noneToDeca',
+      'noneToDeci',
+      'noneToCenti',
+      'noneToMilli',
+      'noneToMicro',
+      'noneToNano',
+      'noneToPico',
+      'noneToFemto',
+      'noneToAtto',
+      'noneToZepto',
+      'noneToYocto'
+    ]);
   });
 
   it('should include length', function() {
