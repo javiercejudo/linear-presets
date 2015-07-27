64a9cd5efe1fcee6ea9e43789463bf7c77a8f050
Javier Cejudo <javier@javiercejudo.com>
2015-07-27 22:43:24 +1000

    refactor(mass): separated as external module

--- src/linear-presets.js
+++ src/linear-presets.js
@@ -4,9 +4,10 @@
 
 var presets = require('../data/presets.json');
 
-presets.metricPrefixes = require('linear-presets-metric-prefixes')
-presets.temperature = require('linear-presets-temperature')
-presets.temperatureDifference = require('linear-presets-temperature-difference')
-presets.distance = require('linear-presets-length')
+presets.metricPrefixes = require('linear-presets-metric-prefixes');
+presets.temperature = require('linear-presets-temperature');
+presets.temperatureDifference = require('linear-presets-temperature-difference');
+presets.distance = require('linear-presets-length');
+presets.mass = require('linear-presets-mass');
 
 exports.PRESETS = presets;
--- test/presets.js
+++ test/presets.js
@@ -54,27 +54,17 @@ describe('built-in presets', function() {
   });
 
   it('should include mass', function() {
-    var mass = presets.mass;
-
-    (10).should.be.exactly(convert(0.01, invert(mass.kilogramToMetricTon)), 'kilogramToMetricTon')
-      .and.exactly(convert(10000, invert(mass.kilogramToGram)), 'kilogramToGram')
-      .and.exactly(convert(1e+7, invert(mass.kilogramToMilligram)), 'kilogramToMilligram')
-      .and.exactly(convert(1e+10, invert(mass.kilogramToMicrogram)), 'kilogramToMicrogram')
-      .and.exactly(convert(0.00984251968503937, invert(mass.kilogramToLongTon)), 'kilogramToLongTon')
-      .and.exactly(convert(0.011023113109243879, invert(mass.kilogramToShortTon)), 'kilogramToShortTon')
-      .and.exactly(convert(1.5747304441776968, invert(mass.kilogramToStone)), 'kilogramToStone')
-      .and.exactly(convert(22.046226218487757, invert(mass.kilogramToPound)), 'kilogramToPound')
-      .and.exactly(convert(352.7396195580167, invert(mass.kilogramToOunce)), 'kilogramToOunce');
-
-    (0).should.be.exactly(convert(0, mass.kilogramToMetricTon), 'kilogramToMetricTon')
-      .and.exactly(convert(0, mass.kilogramToGram), 'kilogramToGram')
-      .and.exactly(convert(0, mass.kilogramToMilligram), 'kilogramToMilligram')
-      .and.exactly(convert(0, mass.kilogramToMicrogram), 'kilogramToMicrogram')
-      .and.exactly(convert(0, mass.kilogramToLongTon), 'kilogramToLongTon')
-      .and.exactly(convert(0, mass.kilogramToShortTon), 'kilogramToShortTon')
-      .and.exactly(convert(0, mass.kilogramToStone), 'kilogramToStone')
-      .and.exactly(convert(0, mass.kilogramToPound), 'kilogramToPound')
-      .and.exactly(convert(0, mass.kilogramToOunce), 'kilogramToOunce');
+    presets.mass.should.have.keys([
+      'kilogramToMetricTon',
+      'kilogramToGram',
+      'kilogramToMilligram',
+      'kilogramToMicrogram',
+      'kilogramToLongTon',
+      'kilogramToShortTon',
+      'kilogramToStone',
+      'kilogramToPound',
+      'kilogramToOunce'
+    ]);
   });
 
   it('should include time', function() {
